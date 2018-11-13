1056331068685b2926ab29fffd8b44dd8371a40e
Ben Li <benbli@gmail.com>
2018-11-13 13:34:27 -0800

    audits, adds svg bg

--- client/src/components/Actionable/Actionable.js
+++ client/src/components/Actionable/Actionable.js
@@ -3,22 +3,24 @@ import './Actionable.css';
 
 const Actionable = () => {
   return (
-    <section className="container flex flex-responsive justify-between align-center text-center">
+    <section className="container flex flex-responsive justify-between align-center text-center bg-svg">
       <div className="tagline-wrapper">
-        <h2>Driving the future of applications</h2>
-        <h4 className="tagline text-muted">Innovation in automotive imagery</h4>
+        <h2 className="text-white">The future of automotive imagery</h2>
+        <h4 className="tagline text-white text-muted">
+          Complete. Easy. Powerful.
+        </h4>
       </div>
       <div className="call-to-action-wrapper flex flex-responsive justify-between">
-        <span className="btn-signup btn-br bg-black text-white upper btn-shadow">
+        <span className="btn-signup btn-br bg-primary text-white upper btn-shadow">
           <a className="btn" href="/signup">
-            Create Free Account
+            Get started
           </a>
         </span>
-        <span className="btn-sales btn-br bg-white upper btn-shadow">
+        {/* <span className="btn-sales btn-br bg-white upper btn-shadow">
           <a className="btn" href="/sales">
             Contact Sales
           </a>
-        </span>
+        </span> */}
       </div>
     </section>
   );
